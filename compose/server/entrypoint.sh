50ba20f775afb3658c3e9c5e4033462fce572e75
= <=>
2019-04-24 14:54:37 +0300

    2344234

--- compose/server/entrypoint.sh
+++ compose/server/entrypoint.sh
@@ -14,12 +14,54 @@ sys.exit(0)
 END
 }
 
+function elasticsearch_ready(){
+python3 << END
+import time
+import json
+import sys
+
+import requests
+
+ELASTICSEARCH_URL = 'http://elasticsearch:9200/'
+SNAPSHOT_REPO = 'backups'
+SNAPSHOT_NAME = 'backup'
+
+index = 'store'
+doc_type = 'product'
+
+health_url = ELASTICSEARCH_URL + '_cat/health'
+
+try:
+    response = requests.get(url=health_url)
+    status_code = response.status_code
+    if status_code == 200:
+
+        restore_url = ELASTICSEARCH_URL + '{repo}/{name}'.format(repo=SNAPSHOT_REPO, name=SNAPSHOT_NAME)
+        requests.post(restore_url)
+
+        sys.exit(0)
+    else:
+        print('Elasticsearch invalid status code: {0}'.format(
+            status_code
+        ))
+        sys.exit(-1)
+except requests.exceptions.RequestException:
+    sys.exit(-1)
+END
+}
+
 
 until postgres_ready; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 >&2 echo "Postgres is up - continuing..."
+
+until elasticsearch_ready; do
+  >&2 echo "Elasticsearch is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "Elasticsearch is up - continuing..."
 
 
 python3 /server/manage.py migrate && \
