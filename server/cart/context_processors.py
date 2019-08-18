793944034dff962d55a76d3293e84d17dc1f3b69
= <=>
2019-08-18 19:45:59 +0300

    temporary

--- server/cart/context_processors.py
+++ server/cart/context_processors.py
@@ -1,8 +1,12 @@
 from .cart import Cart
-
+from dateutil.parser import parse
 
 def cart_processor(request):
     cart = Cart(request)
+    sale = cart.data.get('total_sale', None)
+    if total_sale is None:
+        cart.clear()
+
     return {
         'cart': cart
     }    
