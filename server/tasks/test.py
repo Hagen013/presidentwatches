96da1d3fcbb6929a65a009be1877db57e646528b
= <=>
2019-08-29 16:33:14 +0300

    13231123

--- server/tasks/test.py
+++ server/tasks/test.py
@@ -6,43 +6,43 @@ from shop.models import CategoryPage as Node
 ERRORS_FILE = 'ERRORS.txt'
 
 def test_pages():
-    for node in Node.objects.all():
-        url = 'https://new.presidentwatches.ru/shop/watches/{slug}'.format(
-            slug=node.slug
-        )
-        response = requests.get(url)
-        msg = "{url} {code}".format(
-            url=url,
-            code=response.status_code
-        )
-        print(msg)
-        if response.status_code == 200:
-            pass
-        else:
-            with open(ERRORS_FILE, 'w+') as fp:
-                fp.write(msg)
-        for i in range(1, 9999):
-            page_url = url + '?page={i}'.format(i=i)
-            response = requests.get(page_url)
-            msg = "{url} {code}".format(
-                url=page_url,
-                code=response.status_code
-            )
-            print(msg)
-            if response.status_code == 200:
-                pass
-            elif response.status_code == 404:
-                break
-            else:
-                with open(ERRORS_FILE, 'w+') as fp:
-                    fp.write(msg)
+    # for node in Node.objects.all():
+    #     url = 'https://new.presidentwatches.ru/shop/watches/{slug}'.format(
+    #         slug=node.slug
+    #     )
+    #     response = requests.get(url)
+    #     msg = "{url} {code}".format(
+    #         url=url,
+    #         code=response.status_code
+    #     )
+    #     print(msg)
+    #     if response.status_code == 200:
+    #         pass
+    #     else:
+    #         with open(ERRORS_FILE, 'w+') as fp:
+    #             fp.write(msg)
+    #     for i in range(1, 9999):
+    #         page_url = url + '?page={i}'.format(i=i)
+    #         response = requests.get(page_url)
+    #         msg = "{url} {code}".format(
+    #             url=page_url,
+    #             code=response.status_code
+    #         )
+    #         print(msg)
+    #         if response.status_code == 200:
+    #             pass
+    #         elif response.status_code == 404:
+    #             break
+    #         else:
+    #             with open(ERRORS_FILE, 'w+') as fp:
+    #                 fp.write(msg)
 
     for instance in Product.objects.all():
         url = 'https://new.presidentwatches.ru/watches/{slug}/'.format(
-            slug=node.slug
+            slug=instance.slug
         )
         response = requests.get(url)
-        msg = "{url} {code}".format(
+        msg = "{url} {code}\n".format(
             url=url,
             code=response.status_code
         )
