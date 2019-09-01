ec348bbd0ef54f1bd866d7bfeeef611f6b01bdf9
= <=>
2019-09-01 17:00:34 +0300

    54354

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -283,8 +283,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(days=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=5),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=30),
 }
 # ------------------------------------------------------------------------------
 # SIMPLE_JWT END
